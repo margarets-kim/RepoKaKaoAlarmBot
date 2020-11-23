2d92ae12b45c5158d9ce7b807b2d15abe508753a
Margarets00 <min01134@naver.com>
2020-11-23 13:10:59 +0900

    page

--- page/views.py
+++ page/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class KakaoInfo(APIView):
+    def post(self, request):
+
+        result = reqeust.POST.get('', '')
+        print(request)
+        repository_url = request.POST.get('repoURL', '')
+        user_id = request.POST.get('userRequest.user.id')
+        try:
+            return Response('정상적으로 값이 담겼습니다', status=200)
+        except Exception as e:
+            return Response(str(e), status=404)
