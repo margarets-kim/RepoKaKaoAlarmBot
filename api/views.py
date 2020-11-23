4df0ab40a96f10312185b1ccd61f220ba28c862e
Yubbini <jjas8548@gmail.com>
2020-11-24 01:12:27 +0900

    repoAdd

--- api/views.py
+++ api/views.py
@@ -51,7 +51,7 @@ import json, requests
                 conn.close() """
 
 
-def test(id, fav_repository, nick_name):
+def repoAdd(id, fav_repository, nick_name):
     try:
         conn = None
         if len(id) == 0:
@@ -102,17 +102,11 @@ def barcode(request):
     return_str_git=return_json_str['action']['detailParams']['barcode']['value']
     return_str_id=return_json_str['userRequest']['user']['properties']['plusfriendUserKey']
     return_str_alias="첫번째 레포다"
-    #return_str_alias=return_json_str['action']['detailParams']['barcode']['value']
     return_str_git_barcodeData=json.loads(return_str_git)
 
 
     if return_str == '바코드':
-        test(return_str_id,return_str_git_barcodeData.get("barcodeData"),return_str_alias)
-        print(return_str_git_barcodeData.get("barcodeData"))
-        #print(return_str_git['barcodeData'])
-        #print(return_str_id)
-        #print(return_str_alias)
-        print(test)
+        repoAdd(return_str_id,return_str_git_barcodeData.get("barcodeData"),return_str_alias)
         return JsonResponse({
             'version': "2.0",
             'template': {
