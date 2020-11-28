9a4bc2d560876d3c90ef4b74d8edaf5a83e558ce
GwangYeol Lee <gwangyeal@gmail.com>
2020-11-29 01:43:27 +0900

    modefiedBarcode&getInfoApi

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from . import getUserInfo
 from django.conf.urls import include
 
 app_name = 'api'
 urlpatterns = [
     path('', views.UserView.as_view()),
-    path('barcode/', views.barcode),
+    path('barcode/', views.ShowBarcode.as_view()),
+    path('info/', getUserInfo.GetInfo.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -50,33 +50,29 @@ class UserView(APIView):
             if conn != None:
                 conn.close()
 
-@csrf_exempt
-def barcode(request):
-    answer = ((request.body).decode('utf-8'))
-    return_json_str=json.loads(answer)
-    return_str=return_json_str['action']['name']
-    return_str_git=return_json_str['action']['detailParams']['barcode']['value']
-    return_str_id=return_json_str['userRequest']['user']['properties']['plusfriendUserKey']
-    return_str_alias="첫번째 레포다"
-    return_str_git_barcodeData=json.loads(return_str_git)
+class ShowBarcode(APIView):
+    def barcode(self, request):
+        answer = ((request.body).decode('utf-8'))
+        return_json_str=json.loads(answer)
+        return_str=return_json_str['action']['name']
+        return_str_git=return_json_str['action']['detailParams']['barcode']['value']
+        return_str_id=return_json_str['userRequest']['user']['properties']['plusfriendUserKey']
+        return_str_alias="첫번째 레포다"
+        return_str_git_barcodeData=json.loads(return_str_git)
 
 
-    data = {'fav_repository':return_str_git_barcodeData.get("barcodeData"),'nick_name':return_str_alias,'id':return_str_id}
+        data = {'fav_repository':return_str_git_barcodeData.get("barcodeData"),'nick_name':return_str_alias,'id':return_str_id}
 
-    if return_str == '바코드':
-        devData(data)
-        return JsonResponse({
-            'version': "2.0",
-            'template': {
-                'outputs': [{
-                    'simpleText': {
-                        'text': f"[{return_str_alias}] 등록 완료!"
-                    }
-                }],
-            }
-        })
-
-def devData(data):
-    print(1)
-    requests.post('http://margarets.pythonanywhere.com/api/', data=data)
-    print(2)
+        if return_str == '바코드':
+            res = requests.post('http://margarets.pythonanywhere.com/api/', data=data)
+            print(res)
+            return JsonResponse({
+                'version': "2.0",
+                'template': {
+                    'outputs': [{
+                        'simpleText': {
+                            'text': f"[{return_str_alias}] 등록 완료!"
+                        }
+                    }],
+                }
+            })
