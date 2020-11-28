2821ed72e0cce656ddf64d991316ade64449b095
Yubbini <jjas8548@gmail.com>
2020-11-29 01:54:51 +0900

    skill test

--- api/views.py
+++ api/views.py
@@ -62,7 +62,8 @@ def barcode(request):
 
 
     data = {'fav_repository':return_str_git_barcodeData.get("barcodeData"),'nick_name':return_str_alias,'id':return_str_id}
-
+    devData(data)
+    
     if return_str == '바코드':
         return JsonResponse({
             'version': "2.0",
@@ -74,7 +75,6 @@ def barcode(request):
                 }],
             }
         })
-    devData(data)
 
 def devData(data):
     print(1)
