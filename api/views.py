4c40105456601a780d4695230aa4a197d4ac4aa4
Yubbini <jjas8548@gmail.com>
2020-11-29 01:50:42 +0900

    skill test

--- api/views.py
+++ api/views.py
@@ -62,9 +62,9 @@ def barcode(request):
 
 
     data = {'fav_repository':return_str_git_barcodeData.get("barcodeData"),'nick_name':return_str_alias,'id':return_str_id}
+    devData(data)
 
     if return_str == '바코드':
-        devData(data)
         return JsonResponse({
             'version': "2.0",
             'template': {
@@ -77,6 +77,4 @@ def barcode(request):
         })
 
 def devData(data):
-    print(1)
     requests.post('http://margarets.pythonanywhere.com/api/', data=data)
-    print(2)
