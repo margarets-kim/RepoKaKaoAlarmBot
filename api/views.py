33bc9407715364baa9274bd730f7a8bfe750bd01
Yubbini <jjas8548@gmail.com>
2020-11-28 23:44:59 +0900

    skill test

--- api/views.py
+++ api/views.py
@@ -62,6 +62,9 @@ def barcode(request):
 
 
     if return_str == '바코드':
+        data={'fav_repository':return_str_git_barcodeData.get("barcodeData"),'nick_name':return_str_alias,'id':return_str_id}
+        res=requests.post("http://margarets.pythonanywhere.com/api/", data=data)
+        print(f"error code: {res.status_code}")
 
         return JsonResponse({
             'version': "2.0",
