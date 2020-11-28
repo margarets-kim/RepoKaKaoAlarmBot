01e8c668a318e9167b29b95278f3bec99fc59371
GwangYeol Lee <gwangyeal@gmail.com>
2020-11-28 21:29:11 +0900

    resetApi

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,5 @@ from django.conf.urls import include
 
 app_name = 'api'
 urlpatterns = [
-    #path('', views.UserView.as_view()),
-    path('barcode/', views.barcode),
+    path('', views.UserView.as_view())
 ]
