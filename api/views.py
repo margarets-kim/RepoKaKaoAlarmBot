5cae41f04f930995b591e687960817ac8bb861e4
Yubbini <jjas8548@gmail.com>
2020-11-29 02:42:09 +0900

    skill test

--- api/views.py
+++ api/views.py
@@ -57,15 +57,13 @@ def barcode(request):
     return_str=return_json_str['action']['name']
     return_str_git=return_json_str['action']['detailParams']['barcode']['value']
     return_str_id=return_json_str['userRequest']['user']['properties']['plusfriendUserKey']
-    return_str_alias="첫번째 레포다"
+    return_str_alias="test 레포"
     return_str_git_barcodeData=json.loads(return_str_git)
 
+    data={'fav_repository':return_str_git_barcodeData.get("barcodeData"),'nick_name':return_str_alias,'id':return_str_id}
+    devData(data)
 
     if return_str == '바코드':
-        data={'fav_repository':return_str_git_barcodeData.get("barcodeData"),'nick_name':return_str_alias,'id':return_str_id}
-        res=requests.post("http://margarets.pythonanywhere.com/api/", data=data)
-        print(f"error code: {res.status_code}")
-        
         return JsonResponse({
             'version': "2.0",
             'template': {
@@ -76,3 +74,9 @@ def barcode(request):
                 }],
             }
         })
+
+def devData(data):
+    print(1)
+    res=requests.post('http://margarets.pythonanywhere.com/api/', data=data)
+    print(2)
+    print(f"error code: {res.status_code}")
