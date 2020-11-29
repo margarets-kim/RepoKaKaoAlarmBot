45b33a8f488f78128db9c193dc4d43c52766d9e0
GwangYeol Lee <gwangyeal@gmail.com>
2020-11-29 19:43:42 +0900

    getInfoApi

--- api/views.py
+++ api/views.py
@@ -81,3 +81,42 @@ def devData(data):
     res=requests.post('http://margarets.pythonanywhere.com/api/', data=data)
     print(2)
     print(res.status_code)
+
+class GetInfo (APIView) : 
+    def get (self, request) :
+        fav_repository = request.get.get('fav_repository', '')
+
+        branch_lists = []
+
+        index = fav_repository.find('github')
+        url = fav_repository[index:]
+        index = url.find("/")
+        url_repos = "https://api.github.com/repos"+url[index:]
+        url_branches = "https://api.github.com/repos"+url[index:]+"/branches"
+
+        content_repos = requests.get(url_repos, headers={'Authorization': 'token 6f6d00c786cd3662b25716bf6c6fb6a2084f401d'})
+        jsonObject_repos = json.loads(content_repos.content)
+
+        avatar_url = jsonObject_repos.get("owner").get("avatar_url")
+        name = jsonObject_repos.get("name")
+        created_at = jsonObject_repos.get("created_at")
+        updated_at = jsonObject_repos.get("updated_at")
+        stargazers_count = jsonObject_repos.get("stargazers_count")
+        forks = jsonObject_repos.get("forks")
+
+        content_branches = requests.get(url_branches, headers={'Authorization': 'token 6f6d00c786cd3662b25716bf6c6fb6a2084f401d'})
+        jsonObject_branches = json.loads(content_branches.content)
+        json_size = len(jsonObject_branches)
+
+        for i in range(1, int(json_size)+1):
+            branch_lists.append(jsonObject_branches[i-1].get("name"))
+
+        return Response({
+            "avatar_url" : avatar_url, 
+            "name" : name, 
+            "created_at" : created_at, 
+            "updated_at" : updated_at, 
+            "stargazers_count" : stargazers_count, 
+            "forks" : forks,
+            "branch_lists" : branch_lists
+            }, status = 200)        
