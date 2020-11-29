cd89b904c12cff68c22e3d090358c2e87454bd0d
csh <se>
2020-11-29 21:48:30 +0900

    error 잠시 수정..

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,5 @@ app_name = 'api'
 urlpatterns = [
     path('', views.UserView.as_view()),
     #path('barcode/', views.barcode),
-    path('info/', views.GetRepoInfo.as_view())
+    #path('info/', views.GetRepoInfo.as_view())
 ]
