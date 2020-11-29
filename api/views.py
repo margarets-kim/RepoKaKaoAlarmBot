1b8ac898932bbe767b7850f483f8b1ee6b638a69
Yubbini <jjas8548@gmail.com>
2020-11-29 18:17:33 +0900

    test

--- api/views.py
+++ api/views.py
@@ -100,12 +100,9 @@ def repoList(request):
                     }
                 }],
                 'quickReplies':[{
-                    'type':'block',
                     'label': '입력하기',
-                    'message':'입력하기',
-                    'data':{
-                        'blockId':'5fc34e113ded2277cdfdcab3',
-                    }
+                    'action': 'message',
+                    'messageText':'입력하기',
                 }]
             }
         })
