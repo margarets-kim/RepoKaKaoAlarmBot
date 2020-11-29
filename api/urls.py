80444bdd5b5cd1dc9cbaade43882add54ff81f52
Yubbini <jjas8548@gmail.com>
2020-11-29 17:26:58 +0900

    test

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls import include
 app_name = 'api'
 urlpatterns = [
     path('', views.UserView.as_view()),
-    path('barcode/', views.barcode),
+    #path('barcode/', views.barcode),
     path('repo/', views.repo),
 ]
