28cfda3c2c8f883c93bf8c190488c2759e730760
Yubbini <jjas8548@gmail.com>
2020-11-30 13:31:27 +0900

    test

--- api/views.py
+++ api/views.py
@@ -267,9 +267,11 @@ def barcode(request):
     return_str_git_barcodeData = json.loads(return_str_git)
 
     test=str(return_str_git_barcodeData)
+    test[16]=""
+    test[-2]=""
 
     print(return_str_git_barcodeData)
-    print(test[0])
+    print(test)
     test_return=json.dumps(test)
     test2=json.loads(test_return)
     print(test_return)
